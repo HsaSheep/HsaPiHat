1997e9dcb757ec903e6c9e04636312f4d13d635b
HsaSheep <hsasheep[at]gmail.com>
2018-02-25 18:41:30 +0000

    Scroll_pHAT Dir Add.  - readme.md Add.  - *.py Add

---
+++ Scroll_pHAT/localweather.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+# requires: netifaces for looking up IP in readable way
+# requires: requests human readable HTTP requests
+
+# 要クラス化&selfで引き回し
+
+import json
+import socket
+import time
+import urllib
+from sys import exit
+
+try:
+    import requests
+except ImportError:
+    exit("This script requires the requests module\nInstall with: sudo pip install requests")
+
+import scrollphat
+
+
+class MainApp:
+
+    lotate_time_sec = 0.1      # スクロールの更新間隔(sec)
+    update_time_sec = 60 * 60  # 予報の更新間隔(sec)
+
+    item = ""
+    output = ""
+    weather = ""
+    location = ""
+    location_string = ""
+
+    def get_location(self):
+        res = requests.get('http://ipinfo.io')
+        if res.status_code == 200:
+            json_data = json.loads(res.text)
+            return json_data
+        return {}
+
+    def get_location_now(self):
+        self.location = self.get_location()
+        self.location_string = self.location["city"] + ", " + self.location["country"]
+        print("Location: " + self.location_string)
+
+    # Python 2 vs 3 breaking changes.
+    def encode(self, qs):
+        val = ""
+        try:
+            val = urllib.urlencode(qs).replace("+", "%20")
+        except:
+            val = urllib.parse.urlencode(qs).replace("+", "%20")
+        return val
+
+    def get_weather(self, address):
+        base = "https://query.yahooapis.com/v1/public/yql?"
+        query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"" + address + "\")"
+        qs = {"q": query, "format": "json", "env": "store://datatables.org/alltableswithkeys"}
+
+        uri = base + self.encode(qs)
+
+        res = requests.get(uri)
+        if res.status_code == 200:
+            json_data = json.loads(res.text)
+            return json_data
+        return {}
+
+    def get_weather_days(self, days):
+        self.weather = self.get_weather(self.location_string)
+        self.output = ""
+
+        # Feel free to pick out other data here, for the scrolling message
+        for x in range(0, days):
+            self.item = self.weather["query"]["results"]["channel"]["item"]["forecast"][x]
+            self.output = self.output + " " + self.item["day"] + ": " + self.item["text"] + " - L: " + self.item["low"] + "F - H: " + self.item["high"] + "F"
+
+    def get_weather_info(self):
+        print("\n --- Weather Info ---")
+        print("Get: " + self.weather["query"]["created"][:-1])
+        print("   Location: " + self.location_string)
+        print(self.output)
+        print(" --------------------")
+
+    def scroll_message(self):
+
+        time_count_par_sec = 1 / self.lotate_time_sec
+        time_count = time_count_par_sec * self.update_time_sec
+        while True:
+            try:
+
+                if time_count >= time_count_par_sec * self.update_time_sec:
+                    print("Update...")
+                    self.get_weather_days(7)
+                    self.get_weather_info()
+                    scrollphat.write_string(self.output)
+                    scrollphat.update()
+                    time_count = 0
+
+                scrollphat.scroll()
+                scrollphat.update()
+                time.sleep(self.lotate_time_sec)
+                time_count += 1
+
+            except KeyboardInterrupt:
+                print("\nInterrupted Ctrl + C !!\n")
+                return
+
+
+if __name__ == '__main__':
+    print("Test")
+    ma = MainApp()
+
+    scrollphat.set_brightness(4)
+
+    ma.get_location_now()
+
+    if ma.location["city"] != None:
+        ma.weather = ma.get_weather(ma.location_string)
+
+        ma.scroll_message()
+
+        scrollphat.clear()
+        quit()
---
+++ Scroll_pHAT/readme.md
@@ -0,0 +1,21 @@
+[PI_HAT]:https://shop.pimoroni.com/products/scroll-phat
+
+# Read Me #
+
+All of these files were created for [Scroll pHAT][PI_HAT].
+
+For [Scroll pHAT][PI_HAT] please check [Pimoroni - Scroll pHAT][PI_HAT].
+
+
+_____
+
+
+It is mainly a repository as a work log and backup.
+
+Some or all files may be copied or cloned from the following site.
+
+* [Github - pimoroni/scroll-phat](https://github.com/pimoroni/scroll-phat)
+
+In addition, there is a possibility that modification has been added to these files.
+
+If there is any inconvenience, please contact me because I will remove the file.
